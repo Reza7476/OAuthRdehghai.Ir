10865b845088b21bdbed0d8253d0bb980ed3cfc7
Reza <rdehghani.akorn@gmail.com>
2025-05-04 19:26:41 +0330

    program

--- src/04.Presentation/OAuth.Presentation/Program.cs
+++ src/04.Presentation/OAuth.Presentation/Program.cs
@@ -4,26 +4,19 @@ using OAuth.Presentation.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services
-   .AddDbContext<EFDataContext>(option =>
-   option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    .AddDbContext<EFDataContext>(option =>
+        option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Host.AddAutofac();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-
 app.UseSwagger();
-
 app.UseSwaggerUI(options =>
 {
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
@@ -31,14 +24,17 @@ app.UseSwaggerUI(options =>
 });
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
+
+app.UseRouting();
+app.UseStaticFiles();
 
 app.UseEndpoints(endpoints =>
 {
-    endpoints.MapGet("/", async context =>
+    endpoints.MapGet("/", context =>
     {
         context.Response.Redirect("/swagger/index.html");
+        return Task.CompletedTask; // اضافه کردن این خط برای رفع خطا
     });
     endpoints.MapControllers();
 });
