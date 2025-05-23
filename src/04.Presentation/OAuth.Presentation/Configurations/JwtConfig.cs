41c72d20b632a5e93261d9e48142df714e27f6a5
Reza <rdehghani.akorn@gmail.com>
2025-05-23 10:55:41 +0330

    uninstall jwt token and identity token packages

--- src/04.Presentation/OAuth.Presentation/Configurations/JwtConfig.cs
+++ src/04.Presentation/OAuth.Presentation/Configurations/JwtConfig.cs
@@ -1,33 +1,29 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-
-namespace OAuth.Presentation.Configurations;
+﻿namespace OAuth.Presentation.Configurations;
 
 public static class JwtConfig
 {
     public static IServiceCollection AddJwtAuthontecation(this IServiceCollection services, IConfiguration configuration)
     {
         var jwtKey = configuration["Jwt:Key"];
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
+        //var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
 
-        services.AddAuthentication(options =>
-        {
-            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        })
-        .AddJwtBearer(options =>
-        {
-            options.TokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateIssuer = true,
-                ValidIssuer = "OAuth.rdehghai.ir",
-                ValidateAudience = false,
-                ValidateLifetime = true,
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = key
-            };
-        });
+        //services.AddAuthentication(options =>
+        //{
+        //    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        //    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        //})
+        //.AddJwtBearer(options =>
+        //{
+        //    options.TokenValidationParameters = new TokenValidationParameters
+        //    {
+        //        ValidateIssuer = true,
+        //        ValidIssuer = "OAuth.rdehghai.ir",
+        //        ValidateAudience = false,
+        //        ValidateLifetime = true,
+        //        ValidateIssuerSigningKey = true,
+        //        IssuerSigningKey = key
+        //    };
+        //});
 
         return services;
     }
